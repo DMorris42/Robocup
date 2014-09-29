762eff38d1f2e799f6fb63db1a180e1a3728b2a6
Daniel Morris <dmorris42@hotmail.co.nz>
2014-09-30 02:00:15 +1300

    Fixed it.

--- full_sensing/full_sensing.ino
+++ full_sensing/full_sensing.ino
@@ -1,21 +1,17 @@
 const int numSamples = 5;
 
-static volatile float GP2D120val = 0;
 int GP2D120data[numSamples] = {0};
 int GP2D120dataIndex = 0;
 int GP2D120dist = 0;
 
-static volatile float GP2D12Leftval = 0;
 int GP2D12Leftdata[numSamples] = {0};
 int GP2D12LeftdataIndex = 0;
 int GP2D12Leftdist = 0;
 
-static volatile float GP2D12Rightval = 0;
 int GP2D12Rightdata[numSamples] = {0};
 int GP2D12RightdataIndex = 0;
 int GP2D12Rightdist = 0;
 
-static volatile float LongIRval = 0;
 int LongIRdata[numSamples] = {0};
 int LongIRdataIndex = 0;
 int LongIRdist = 0;
@@ -32,55 +28,58 @@ GP2Y0A02YK on pin 2
 */
 
 void setup() {
+  Serial.begin(9600);
 }
 
 void loop() {
-  GP2D12Leftval = read_gp2d12_range(GP2D12Leftpin);
-  if (GP2D12Leftval != -1) {
-    GP2D12Leftdata[GP2D12LeftdataIndex++] = GP2D12Leftval;
-  }
+  GP2D12Leftdata[GP2D12LeftdataIndex++] = read_gp2d12_range(GP2D12Leftpin);
   
-  GP2D12Rightval = read_gp2d12_range(GP2D12Rightpin);
-  if (GP2D12Rightval != -1) {
-    GP2D12Rightdata[GP2D12RightdataIndex++] = GP2D12Rightval;
-  }
+  GP2D12Rightdata[GP2D12RightdataIndex++] = read_gp2d12_range(GP2D12Rightpin);
   
-  LongIRval = read_IR_long_range(LongIRpin);
-  if (LongIRval != -1) {
-    LongIRdata[LongIRdataIndex++] = LongIRval;
-  }
+  LongIRdata[LongIRdataIndex++] = read_IR_long_range(LongIRpin);
   
-  GP2D120val = read_gp2d120_range(GP2D120pin);
-  if (GP2D120val != -1) {
-    GP2D120data[GP2D120dataIndex++] = GP2D120val;
-  }
+  GP2D120data[GP2D120dataIndex++] = read_gp2d120_range(GP2D120pin);
   
   if (GP2D120dataIndex == numSamples) {
     GP2D120dist = sum(GP2D120data)/GP2D120dataIndex;
+    Serial.print("GP2D120 Range: ");
+    Serial.println(GP2D120dist, DEC);
     GP2D120dataIndex = 0;
   }
   
   if (GP2D12LeftdataIndex == numSamples) {
     GP2D12Leftdist = sum(GP2D12Leftdata)/GP2D12LeftdataIndex;
+    Serial.print("GP2D12 Left Range: ");
+    Serial.println(GP2D12Leftdist, DEC);
     GP2D12LeftdataIndex = 0;
   }
   
   if (GP2D12RightdataIndex == numSamples) {
     GP2D12Rightdist = sum(GP2D12Rightdata)/GP2D12RightdataIndex;
+    Serial.print("GP2D12 Right Range: ");
+    Serial.println(GP2D12Rightdist, DEC);
     GP2D12RightdataIndex = 0;
   }
   
   if (LongIRdataIndex == numSamples) {
     LongIRdist = sum(LongIRdata)/LongIRdataIndex;
+    Serial.print("Long IR Range: ");
+    Serial.println(LongIRdist, DEC);
     LongIRdataIndex = 0;
   }
+  delay(300);
 }
 
 int sum(int* dataArray) {
   int i;
   int sum = 0;
   for (i = 0; i < numSamples; i++) {
-    sum += *dataArray++;
+    if (*dataArray != -1) {
+      sum += *dataArray++;
+    }
+  }
+  if (sum == 0) {
+    sum = -1;
   }
   return sum;
 }
